221a2ec4777df3ab24c36f2d41133aee855dafe2
masonAIT <masoncch@users.noreply.github.com>
2020-06-15 21:22:38 +1000

    Added App Icon and implemented FaceBook Login

--- app/src/main/java/com/example/prototype_v2/MainActivity.java
+++ app/src/main/java/com/example/prototype_v2/MainActivity.java
@@ -12,6 +12,15 @@ import android.widget.Toast;
 
 import com.example.prototype_v2.activities.HomeActivity;
 import com.example.prototype_v2.database.DatabaseHelper;
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -20,12 +29,19 @@ import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity{
     DatabaseHelper myDb;
     Button loginBtn;
+    LoginButton fbLoginBtn;
     int RC_SIGN_IN = 0;
     SignInButton signInButton;
     GoogleSignInClient mGoogleSignInClient;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +51,9 @@ public class MainActivity extends AppCompatActivity{
         myDb = new DatabaseHelper(this);
 
         signInButton = findViewById(R.id.googleSignInBtn);
+        fbLoginBtn = findViewById(R.id.login_button);
         loginBtn = findViewById(R.id.loginBtn);
+        callbackManager = CallbackManager.Factory.create();
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,6 +74,25 @@ public class MainActivity extends AppCompatActivity{
                 signIn();
             }
         });
+        fbLoginBtn.setReadPermissions(Arrays.asList("email"));
+
+        fbLoginBtn.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Intent signInIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(signInIntent);
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
     }
 
     private void signIn(){
@@ -63,15 +100,52 @@ public class MainActivity extends AppCompatActivity{
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
+        callbackManager.onActivityResult(requestCode, resultCode, data);
         if(requestCode == RC_SIGN_IN){
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             handleSignInResult(task);
         }
     }
+/*
+    AccessTokenTracker tokenTracker = new AccessTokenTracker() {
+        @Override
+        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+            if (currentAccessToken == null){
+                Toast.makeText(MainActivity.this, "User Logged Out", Toast.LENGTH_LONG).show();
+            }
+            else {
+                loadUserProfile(currentAccessToken);
+            }
+        }
+    };
+
+    private void loadUserProfile(AccessToken newAccessToken){
+        GraphRequest request = GraphRequest.newMeRequest(newAccessToken, new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                try {
+                    String first_name = object.getString("first_name");
+                    String last_name = object.getString("last_name");
+                    String email = object.getString("email");
+                    String id = object.getString("id");
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "first_name, last_name, email, id");
+        request.setParameters(parameters);
+        request.executeAsync();
+    }
+
+ */
 
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
--- app/src/main/java/com/example/prototype_v2/activities/HomeActivity.java
+++ app/src/main/java/com/example/prototype_v2/activities/HomeActivity.java
@@ -73,7 +73,7 @@ public class HomeActivity extends AppCompatActivity {
             String personName = acct.getDisplayName();
 
             nameText.setText(personName);
-            
+
         }
 
         signOutBtn.setOnClickListener(new View.OnClickListener() {
